2a4718e171d58377445f305ef1cafd0c55e73d3b
nahiankhondokar <info.nahian13@gmail.com>
2025-01-01 19:53:15 +0600

    product list show

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,21 +9,30 @@
             <th scope="col">Name</th>
             <th scope="col">Description</th>
             <th scope="col">Price</th>
+            <th scope="col">Quantity</th>
             <th scope="col">Action</th>
           </tr>
         </thead>
         <tbody>
+          @forelse($products as $index => $product)
           <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
+            <th scope="row">{{$index + 1}}</th>
+            <td>{{$product->name}}</td>
+            <td>{{$product->desc}}</td>
+            <td>{{$product->price}}</td>
+            <td>{{$product->quantity}}</td>
             <td>
                 <a href=""><i class="fa-solid fa-eye"></i></a>
                 <a href=""><i class="fa-solid fa-pen-to-square"></i></a>
                 <a href=""><i class="fa-solid fa-trash"></i></a>
             </td>
           </tr>
+
+          @empty 
+          <tr>
+            <td colspan="6" class="text-danger font-weight-bold text-center">Not Data Found !</td>
+          </tr>
+          @endforelse
         </tbody>
     </table>
 </section>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('product.view');
-});
+Route::get('/', [ProductController::class, 'index']);
+Route::resource('product', ProductController::class);
